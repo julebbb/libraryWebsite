33d18670427876e143c1edae2159e8c9db6ebda6
Justine Lebbrecht <justinelebbrecht@hotmail.fr>
2019-01-08 11:02:39 +0100

    create synfony structure and add loading page

---
+++ public/dist/js/main.js
@@ -0,0 +1,37 @@
+let paragraph = $("#loadingP");
+let line = $('.line');
+let divLoad = $("#divLoad");
+
+//Loading page animation
+jQuery(document).ready(function ($) {
+    line.css('height', '100vh');
+    line.css('border-radius', '0');
+
+    paragraph.css("opacity", 0);
+
+    if (paragraph.css("opacity", 0)) {
+
+        setTimeout(function () {
+            paragraph.text('Ready !');
+            paragraph.css("opacity", 1);
+        }, 500);
+    }
+
+
+
+    if (line.css('height', '100vh')) {
+        setTimeout(function () {
+            line.css('width', '100vw');
+            paragraph.css('background', 'transparent')
+            divLoad.css('opacity', 0);
+            if (divLoad.css('opacity', 0)) {
+                setTimeout(function () {
+                    divLoad.remove();
+
+                }, 3000);
+            }
+
+        }, 1000);
+
+    }
+});
